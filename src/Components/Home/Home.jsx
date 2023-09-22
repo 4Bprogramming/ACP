010aced6d259cd8e8d1bcc6d04bfb8f4991f1418
neleon10 <cvn45@hotmail.com>
2023-09-23 01:25:45 +0200

    media query file added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,31 +1,43 @@
 import React from "react";
-import "./Home.css";
 import logo from "../../Assets/logo.png";
 import pict from "../../Assets/program.jpeg";
 import pict1 from "../../Assets/reuniones.jpg";
 import line from "../../Assets/line.png";
+import "./Home.css";
+import "./mediaHome.css";
 
 function Home() {
   return (
-    <div>
-      <div className="AboutBody">
-        <div className="AboutContainer">
-          <div className="StyledHeading">C O N S U L T O R A</div>
-          <img className="fotoo" src={line} alt="linea" />
-          <div className="TextSection">
-            <img className="Photo2" src={logo} />
-           
-          </div>
+    <>
+      <div className="aboutBody">
+        <div>
+          
         </div>
-        <div className="position">
-              <button className="btninfo">Más info aquí</button>
-            </div>
-        <div className="AboutContainer">
-          <img className="Photo" src={pict1} />
+        <div>
+          <button className="btninfo">Más info aquí</button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
 export default Home;
+
+// <div>
+//       <div className="AboutBody">
+//         <div className="AboutContainer">
+//           <div className="StyledHeading">C O N S U L T O R A</div>
+//           <img className="fotoo" src={line} alt="linea" />
+//           <div className="TextSection">
+//             <img className="Photo2" src={logo} />
+
+//           </div>
+//         </div>
+//         <div className="position">
+//               <button className="btninfo">Más info aquí</button>
+//             </div>
+//         <div className="AboutContainer">
+//           <img className="Photo" src={pict1} />
+//         </div>
+//       </div>
+//     </div>
