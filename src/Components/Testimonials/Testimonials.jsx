5ca5ffea28ff5c0da06456e400b8e7a29e63613e
gabdala17 <gisella.abdala@gmail.com>
2023-10-30 20:30:34 -0300

    testimonials

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,26 +1,37 @@
 import React, { useState } from "react";
 import "./Testimonials.css";
 import comilla from "../../Assets/comilla.png";
+import eco from "../../Assets/testimonials/ecocuyum.jpg"
+import oliver from "../../Assets/testimonials/oliver.jpg"
+import oscar from "../../Assets/testimonials/OscarNiemetz.jpg"
+import rustico from "../../Assets/testimonials/rusticos_mendoza.jpg"
+
 
 function Testimonials() {
   const [Testimonials] = useState([
     {
-      src: <img src={comilla} width="80px" height="40px" />,
+      src: <img src={oliver} width="80px" height="40px" />,
       title: "Florencia Manzanel, Grupo J B Justo",
       desc: "Tuvimos una excelente experiencia trabajando con la consultora ACP, hicieron un análisis completo de nuestra cafeteria, nos hicieron una propuesta de organización y obtuvimos excelentes resultados con ello. Estamos totalmente satisfechos con su trabajo.",
       active: false,
     },
     {
-      src: <img src={comilla} width="80px" height="40px" />,
+      src: <img src={rustico} width="80px" height="40px" />,
       title: "Martin Aguero. Rusticos Mendoza",
       desc: "Me ayudaron mucho a re organizar mi empresa desde cero, muy exhaustivo el diagnostico, lo cual me permitió enforcarme en las areas que estaban sin organización y dirección.",
       active: true,
     },
     {
-      src: <img src={comilla} width="80px" height="40px" />,
+      src: <img src={oscar} width="80px" height="40px" />,
       title: "Oscar Niemetz.  @coachingconoscarniemetz",
       desc: "ACP me acompañó y me acompaña en aspectos específicos de comercialización de servicios, difusión y ampliación de mercados. Han sido particularmente innovadores en procedimientos lo que se ha evidenciado en el crecimiento de ventas en pocos meses.",
       active: false,
+    },
+    {
+      src: <img src={eco} width="80px" height="40px" />,
+      title: "Armando Parlanti. Eco Cuyum",
+      desc: "Actualmente estamos planificando nuestra desarrollo de imagen corporativa y desarrollo publicitario con ACP y ya estamos implementando la venta online, gracias a la colaboración de la consultora.",
+      active: true,
     },
   ]);
   return (
